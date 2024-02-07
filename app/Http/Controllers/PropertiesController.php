b13b2b5650df5f7035c2a61dcc736e6ceeddd63f
Tumelo Petsane <87595329+Petsane@users.noreply.github.com>
2024-02-07 10:36:46 +0200

    Revert "Side bar feature b"

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\properties;
 use Illuminate\Http\Request;
 
 class PropertiesController extends Controller
@@ -14,28 +13,7 @@ class PropertiesController extends Controller
      */
     public function index()
     {
-        $AllPropertiesRec = properties::get()->toArray();
-        $records = [];
-        $i = 0;
-
-        foreach($AllPropertiesRec as $propertiesRec){
-            $records[$i]['id'] = $propertiesRec['id'];
-            $records[$i]['kodisno'] = $propertiesRec['kodisno'];
-            $records[$i]['plotno'] = $propertiesRec['plotno'];
-            $records[$i]['bonds'] = $propertiesRec['bonds'];
-            $records[$i]['addr1'] = $propertiesRec['addr1'];
-            $records[$i]['addr2'] = $propertiesRec['addr2'];
-            $records[$i]['addr3'] = $propertiesRec['addr3'];
-            $records[$i]['pcode'] = $propertiesRec['pcode'];
-            $records[$i]['occupationdate'] = $propertiesRec['occupationdate'];
-            $records[$i]['saledate'] = $propertiesRec['saledate'];
-            $records[$i]['otherproperty'] = $propertiesRec['otherproperty'];
-            $records[$i]['numberowners'] = $propertiesRec['numberowners'];
-            $records[$i]['datevacated'] = $propertiesRec['datevacated'];
-            $records[$i]['transferdate'] = $propertiesRec['transferdate'];
-            $i++;
-        }
-        return view('property',compact('records'));
+        //
     }
 
     /**
